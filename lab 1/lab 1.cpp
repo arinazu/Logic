748d542c79ec7fffebeb3ec0b9d02ba169481fda
Arina <zubr230305@gmail.com>
2023-09-11 21:54:16 +0300

    Лабораторная работа 1

---
+++ lab 1/lab 1.cpp
@@ -0,0 +1,63 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+
+    int a[5][5];
+
+    //Задание 2. Инициализация массива случайными числами
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+
+            a[i][j] = rand() - rand();
+            printf("%8d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    //Задание 1. Разница между макс и мин элементами массива
+
+    int max = a[0][0];
+    int min = a[0][0];
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (max < a[i][j])
+            {
+                max = a[i][j];
+            }
+            if (min > a[i][j])
+            {
+                min = a[i][j];
+            }
+        }
+    }
+    printf("\n");
+
+    int d = max - min;
+    printf("max = %d, min = %d, разность = %d\n", max, min, d);
+
+    //Задание 4. Сумма значений каждой строки двумерного массива
+
+    int sum;
+    for (int i = 0; i < 5; i++)
+    {
+        sum = 0;
+        for (int j = 0; j < 5; j++)
+        {
+            sum += a[i][j];
+        }
+        printf("строка %d = %d \n", i, sum);
+    }
+    return 0;
+}
---
+++ lab1_2/lab1_2.cpp
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct Student
+{
+    int num;
+    char firstname[20];
+    char name[20];
+    char faculty[20];
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+
+    //Задание 3 + Задание 5. Ввод массива произвольного размера и поиск структур
+
+    int n;
+    printf("Введите количество студентов:");
+    scanf("%d", &n);
+
+    Student* a;
+    a = (Student*)malloc(n * sizeof(Student));
+
+    for (int i = 0; i < n; i++)
+    {
+        a[i].num = 100 + i;                             
+        snprintf(a[i].firstname, 20, "Фамилия%d", i);
+        snprintf(a[i].name, 20, "Имя%d", i);
+        snprintf(a[i].faculty, 20, "Факультет%d", i);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%4d", a[i].num);
+        printf("%15s", a[i].firstname);
+        printf("%15s", a[i].name);
+        printf("%20s", a[i].faculty);
+        printf("\n");
+    }
+
+    int m;
+    printf("\n");
+    printf("Введите номер зачетки: ");
+    scanf("%d", &m);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (m == a[i].num)
+        {
+            printf("%4d", a[i].num);
+            printf("%15s", a[i].firstname);
+            printf("%15s", a[i].name);
+            printf("%20s", a[i].faculty);
+            printf("\n");
+        }
+    }
+
+    free(a);
+    return 0;
+}
